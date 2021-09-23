c77dc8f3db3d729bf17786cb87758d8035229d21
Fer96rg <91261327+Fer96rg@users.noreply.github.com>
2021-09-23 14:48:18 +0200

    Delete fun_max_theta.m

--- fun_max_theta.m
+++
@@ -1,7 +0,0 @@
-function [max_theta] = fun_max_theta(M1,gamma)
-beta=0:1:90;
-N=length(beta);
-for i=1:N
-    theta(i)=atand((M1^2*(sind(beta(i)))^2-1)*2*cotd(beta(i))/(gamma*M1^2+M1^2*cosd(2*beta(i))+2));
-end
-max_theta=max(theta);
